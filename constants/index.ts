61c052b18bcf4b201ac75541abc815528da7bba2
druzxh <badrudin.on@gmail.com>
2024-03-30 17:48:19 +0700

    init app

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sideBarLinks = [
+    {
+        label: "Home",
+        route: "/",
+        imgUrl : "/icons/Home.svg"
+    },
+    {
+        label: "Upcoming",
+        route: "/upcoming",
+        imgUrl : "/icons/Upcoming.svg"
+    },
+    {
+        label: "Previous",
+        route: "/previous",
+        imgUrl : "/icons/Previous.svg"
+    },
+    {
+        label: "Recording",
+        route: "/recording",
+        imgUrl : "/icons/Recording.svg"
+    },
+    {
+        label: "Personal Room",
+        route: "/personal-room",
+        imgUrl : "/icons/Personal-Room.svg"
+    }
+]
